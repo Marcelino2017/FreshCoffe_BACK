8e73ee2143b5cdcadc4a8a7b8f8569fe11587f53
Marcelino <marcelinomeba@hotmail.com>
2023-09-02 11:26:42 -0500

    Se realiza la funcionalidad del login y logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,18 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\ResgistroRuquest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function register(ResgistroRuquest $resquest)
+    public function register(ResgistroRuquest $request)
     {
         $user = User::create([
-            'name' => $resquest->name,
-            'email' => $resquest->email,
-            'password' => bcrypt($resquest->password),
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
         ]);
 
         return [
@@ -22,13 +24,34 @@ class AuthController extends Controller
         ];
     }
 
-    public function login(Request $resquest)
+    public function login(LoginRequest $request)
     {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
 
+        if (!Auth::attempt($credentials, true)) {
+            return response([
+                'errors' => ['El email o  el password son incorrectos']
+            ], 422);
+        }
+
+        $user = Auth::user();
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user,
+        ];
     }
 
-    public function logout(Request $resquest)
+    public function logout(Request $request)
     {
+        $user = $request->user();
+        //Revocar el token de acceso actual del usuario
+        $user->currentAccessToken()->delete();
 
+        return [
+            'user' => null
+        ];
     }
 }
