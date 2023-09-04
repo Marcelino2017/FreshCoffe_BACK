082080fb73b3170ff5a448fcead2e5ce011637f9
Marcelino <marcelinomebe@gmail.com>
2023-09-04 15:39:20 -0500

    Mostrando el Contenido del Pedido

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -16,6 +16,10 @@ class Pedido extends Model
 
     public function productos()
     {
-        return $this->belongsToMany(Producto::class, 'pedido_productos');
+        /*
+        * Relación de mucho a mucho con taba pivote
+        * indicamos que otro campo de la tabla pivote se desea traer
+        */
+        return $this->belongsToMany(Producto::class, 'pedido_productos')->withPivot('cantidad');
     }
 }
